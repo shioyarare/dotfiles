9647e88795b4475e5fb841425060ad03e7276de8
Shoya HARA <shioyarare@gmail.com>
2021-02-12 03:19:22 +0900

    feat: add setup shell script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOT_FILES = (".vimrc" ".config")
+CUR_DIRES = `pwd`
+for file in ${DOT_FILES[@]}
+do 
+    ln -s -f $CUR_DIR/$file $HOME/$file
+done
+
+DEIN_FILES = ("init.vim" "dein.toml" "dein_lazy.toml")
+
+if [[ ! -e $HOME/.config ]]; then
+    mkdir $HOME/.config/nvim
+fi 
+
+for file in ${DEIN_FILES[@]}
+do
+    ln -s -f $CUR_DIR/$file $HOME/.config/nvim/
+done
