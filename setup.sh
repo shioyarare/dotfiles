f69eb6d5c7f05cfdfd9045942242eef1e1a371c8
Shoya HARA <shioyarare@gmail.com>
2021-02-12 03:30:22 +0900

    fix: shell script syntax error

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-DOT_FILES = (".vimrc" ".config")
-CUR_DIRES = `pwd`
+sudo apt install neovim
+DOT_FILES=(".vimrc" ".config")
+CUR_DIRES=`pwd`
 for file in ${DOT_FILES[@]}
 do 
     ln -s -f $CUR_DIR/$file $HOME/$file
 done
 
-DEIN_FILES = ("init.vim" "dein.toml" "dein_lazy.toml")
+DEIN_FILES=("init.vim" "dein.toml" "dein_lazy.toml")
 
 if [[ ! -e $HOME/.config ]]; then
     mkdir $HOME/.config/nvim
